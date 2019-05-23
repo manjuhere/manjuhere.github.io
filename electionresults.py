b01ae6f4f6a90aa046c4322788f9c8e59252c67a
Manjunath C <manjuhere@me.com>
2019-05-23 11:24:39 +0530

    removed google tag

--- electionresults.py
+++ electionresults.py
@@ -122,17 +122,17 @@ while True:
 
         file.write("\n\n")         
 
-        file.write("""
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src='https://www.googletagmanager.com/gtag/js?id=UA-138371535-2'></script>
-        <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+        # file.write("""
+        # <!-- Global site tag (gtag.js) - Google Analytics -->
+        # <script async src='https://www.googletagmanager.com/gtag/js?id=UA-138371535-2'></script>
+        # <script>
+        # window.dataLayer = window.dataLayer || [];
+        # function gtag(){dataLayer.push(arguments);}
+        # gtag('js', new Date());
 
-        gtag('config', 'UA-138371535-2');
-        </script>
-        """) 
+        # gtag('config', 'UA-138371535-2');
+        # </script>
+        # """) 
 
         file.close() 
 
